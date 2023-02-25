15f9282832834eb4af733fba81a7a271a2a8184c
GDWD-KC <110142726+GDWD-KC@users.noreply.github.com>
2023-02-24 22:38:20 -0500

    2

---
+++ practice/js/app.js
@@ -0,0 +1,12 @@
+// Console Log
+console.log("AYO")
+
+// Alert
+// alert("HEY")
+
+// document.getElementById("demo").style.color = "red";
+let demo = document.getElementById("demo");
+
+demo.innerHTML = "I changed!!! AHAAHAHA";
+
+demo.style.color = "red";
